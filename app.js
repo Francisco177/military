cfd14b71868d3b52bf3095d347a4c40762c7d759
emmyzee45 <emmyaondohemba45@gmail.com>
2025-05-06 18:15:45 +0100

    Fix MongoDB URI and file naming issues

--- app.js
+++ app.js
@@ -15,14 +15,10 @@ const app = express();
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB:', err);
-});
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Error connecting to MongoDB:", err));
 
 // Admin credentials
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
